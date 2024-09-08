090e14181a4cd2f49a961f0b16c9a57f70883f9d
cclxionger <c3478034982@163.com>
2024-09-08 11:49:44 +0800

    使用了结构体tag 替换了代码校验，但是不能保证密码俩次一致

--- models/parms.go
+++ models/parms.go
@@ -1,6 +1,7 @@
 package models
-type ParamSignUp struct{
-	Username string `json:"username"`
-	Password string `json:"password"`
-	RePassword string `json:"re_password"`
+
+type ParamSignUp struct {
+	Username   string `json:"username" binding:"required,min=3"`
+	Password   string `json:"password" binding:"required,min=6"`
+	RePassword string `json:"re_password" binding:"required"`
 }
